7f51bf8cf639fc84ed21e5bd6886cfdb2e9f0b22
George <geosem042@gmail.com>
2025-02-06 21:24:57 +0200

    test 2

--- resources/views/pages/filamentor.blade.php
+++ resources/views/pages/filamentor.blade.php
@@ -1,6 +1,6 @@
 <x-filament::page>
     <div>
-        <h1>Welcome to Filamentor2!</h1>
+        <h1>Welcome to Filamentor3!</h1>
         <p>Your page builder is ready.</p>
     </div>
 </x-filament::page>
